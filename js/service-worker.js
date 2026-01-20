e7aab2aec5c277ffa5e5cb617cd60e5c3f5ad2ef
ppfunusabangsa <ppfumedia@gmail.com>
2026-01-21 02:32:31 +0700

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pesantren-cache-v4";
+const CACHE_NAME = "pesantren-cache-v5";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -22,4 +22,5 @@ self.addEventListener("fetch", event => {
   );
 
 });
+
 
